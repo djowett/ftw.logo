e388a769e160cd907e4d2174cca03f4bedd3d822
Daniel Jowett <daniel@jowettenterprises.com>
2018-06-30 17:38:51 +0100

    Revert "Try SingletonNameChooser"
    This reverts commit b3aaecafed6857541c2de2c699d2bbb56075e37b.

--- ftw/logo/manual_override.py
+++ ftw/logo/manual_override.py
@@ -4,8 +4,6 @@ from plone.namedfile.field import NamedBlobImage
 from plone.supermodel import model
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope import schema
-from zope.container.interfaces import INameChooser
-from zope.interface import implements
 
 from ftw.logo import _
 
@@ -97,16 +95,3 @@ class AddManualOverrideForm(add.DefaultAddForm):
 
 class AddManualOverrideView(add.DefaultAddView):
     form = AddManualOverrideForm
-
-class SingletonNameChooser(object):
-    """ Always chooses the same ID for an item"""
-    implements(INameChooser)
-
-    def __init__(self, context):
-        pass
-
-    def chooseName(self, name, object):
-        return u"newid"
-
-    def checkName(self, name, object):
-        return True
