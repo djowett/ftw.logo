7ae4a48785cbaa6b87b10c551e401e6cc5f5fcc6
Daniel Jowett <daniel@jowettenterprises.com>
2018-08-08 01:11:58 +0100

    Fixup LogoConfigOverride; change self.base to Image; alter Image class to accept kwargs

--- ftw/logo/image.py
+++ ftw/logo/image.py
@@ -3,18 +3,19 @@ from wand.image import Image
 
 class Image(Image):
 
-    def __init__(self, filename, extension):
-        self.filename = filename
+    def __init__(self, **kwargs):
+        self.filename = kwargs.get("filename", None)
         # behind format is a setter so the extension has to be kept seperately
-        self.extension = extension
+        self.extension = kwargs.pop("extension", None)
 
-        super(Image, self).__init__(filename=self.filename)
-        self.format = self.extension
+        super(Image, self).__init__(**kwargs)
+        if self.extension:
+            self.format = self.extension
 
     def make_blob(self):
         # wand.py is not able to convert mvg to svg
-        if self.extension == 'svg':
-            return open(self.filename, 'r').read()
+        if self.extension == 'svg' and self.filename:
+            return self.filename.read()
         return super(Image, self).make_blob(self.extension)
 
     def append(self, other):
